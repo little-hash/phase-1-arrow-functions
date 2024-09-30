4fa26021d7a73e3c8811bb723ea0e31288618075
little-hash <ezraraddai166@gmail.com>
2024-09-30 16:23:39 +0300

    firstcommit

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(){
+    let divides = 2000
+    let divide = 100
+    const anser = divides / divide
+    return anser
+}
+const square = (num = 2) => {
+    let parameter = 2
+    const anser = parameter * num;
+    return anser
+}
+const add = (add) =>{
+    let parameter1 = 3
+    let parameter2 = 4
+    const anser = Math.abs(parameter1 + parameter2)
+    return anser
+}
